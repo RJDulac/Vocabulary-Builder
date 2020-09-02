ca698c94ced3868efd65c48d76d24f833f3a46c3
machine <analyticrye@gmail.com>
2020-09-02 01:24:55 -0400

    destructuring and formatting

--- src/Components/PracticeWords.js
+++ src/Components/PracticeWords.js
@@ -5,32 +5,42 @@ class PracticeWords extends Component {
   static defaultProps = {
     vocabList: [{ word: "default", definition: "none" }],
   };
+
   static propTypes = {
     vocabList: PropTypes.array.isRequired,
   };
+
   state = {
     showAnswer: false,
     randomVocab: this.randomVocab(),
   };
+
   randomVocab() {
     return this.props.vocabList[
       Math.floor(Math.random() * this.props.vocabList.length)
     ];
   }
+
   handleRandomVocab = () =>
     this.setState({ randomVocab: this.randomVocab(), showAnswer: false });
+
   handleShowAnswer = () =>
     this.setState({ showAnswer: !this.state.showAnswer });
+
   render() {
-    const { randomVocab, showAnswer } = this.state;
+    const {
+      randomVocab: { word, definition },
+      showAnswer,
+    } = this.state;
+
     return (
       <div style={{ textAlign: "center" }}>
         <button onClick={this.handleShowAnswer}>
           {!showAnswer ? <span>Show Answer</span> : <span>Hide Answer</span>}
         </button>
         <button onClick={this.handleRandomVocab}>Next Random Word</button>
-        <p>What is the definition of {randomVocab.word}?</p>
-        {showAnswer && <p>{randomVocab.definition}.</p>}
+        <p>What is the definition of {word}?</p>
+        {showAnswer && <p>{definition}.</p>}
       </div>
     );
   }
